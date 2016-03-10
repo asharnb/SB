9d46fc6932e06f7794bec6a0671fe6de7e95ad44
krknth <krknth@gmail.com>
2016-03-10 17:46:57 +0530

    Custom css & js files for live shooting page.

---
+++ modules/custom/studiobridge_store_images/js/studio-bridge-auto-submit.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(function() {
+        setInterval(function() {
+            document.getElementById('views-exposed-form-individual-project-view-page-1').submit();
+        }, 10000);
+    });
+})(jQuery);
+
+
