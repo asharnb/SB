7547e2d2ebce6cf6ae2c5de6bb198d2cb006f19a
Ashar Babar <asharbabar@Ashars-MacBook-Pro.local>
2016-04-17 20:51:40 +0400

    Added color coding for delete functions

--- modules/custom/studiobridge_live_shoot_page/js/scripts.js
+++ modules/custom/studiobridge_live_shoot_page/js/scripts.js
@@ -3,11 +3,18 @@
 
     Drupal.behaviors.awesome = {
         attach: function(context, settings) {
-            console.log('hiiii');
 
-            if ($('#del-img-636').prop("checked")==true){
-                console.log('checked');
-            }
+            $('input:checkbox').change(function(){
+                var closeelement = document.getElementById( "ribboncontainer" );
+                if($(this).is(":checked")) {
+                    var id = $(this).parents('span').attr('id')
+                    $('#seq-'+id).addClass('tag-deleted');
+                    console.log($('#tag-img-'+id))
+                } else {
+                    var id = $(this).parents('span').attr('id')
+                    $('#seq-'+id).removeClass('tag-deleted');
+                }
+            });
     }
     };
 
