e0f7ac0a3d247d1ca445f2e4e356b2dd546542f5
Ashar Babar <asharbabar@Ashars-MacBook-Pro.local>
2016-04-20 16:10:00 +0400

    working copy upload

--- modules/custom/studiobridge_live_shoot_page/js/scripts.js
+++ modules/custom/studiobridge_live_shoot_page/js/scripts.js
@@ -1,18 +1,23 @@
 (function ($) {
+
+    //TO DO: disable delete button by default, only enable when something is checked to delete
+
     'use strict';
 
-    Drupal.behaviors.awesome = {
+    Drupal.behaviors.tagfordelete = {
         attach: function(context, settings) {
 
             $('input:checkbox').change(function(){
                 var closeelement = document.getElementById( "ribboncontainer" );
                 if($(this).is(":checked")) {
-                    var id = $(this).parents('span').attr('id')
+                    var id = $(this).parents('span').attr('id');
                     $('#seq-'+id).addClass('tag-deleted');
-                    console.log($('#tag-img-'+id))
+                    $('#seq-'+id).html("<i class='fa fa-trash'></i>")
+                    console.log($('#tag-img-'+id).html)
                 } else {
                     var id = $(this).parents('span').attr('id')
                     $('#seq-'+id).removeClass('tag-deleted');
+                    $('#seq-'+id).html($('#seq-'+id).attr('name'))
                 }
             });
     }
